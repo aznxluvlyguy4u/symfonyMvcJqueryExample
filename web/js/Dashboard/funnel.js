6a2a9f8a2422b79d9268eabe9f812e12c68821df
Yubin Chen <yubinchen@jvt-nautilus.local>
2017-05-29 18:41:02 +0200

    Fix drag and drop for now

--- web/js/Dashboard/funnel.js
+++ web/js/Dashboard/funnel.js
@@ -1,23 +1,27 @@
 function drag(event) {
     event.dataTransfer.setData("elementId", event.target.id);
+    event.dataTransfer.setData("oldStatusId", event.target.parentElement.id);
 }
 
 function allowDrop(event) {
-    //dropping is allowed
-    if ($(event.target).hasClass("leads-status")) {
-        event.preventDefault();
-        event.dataTransfer.dropEffect = "all";
-    }
-    //dropping is NOT allowed
-    else {
-        event.dataTransfer.dropEffect = "none";
-    }
+    // //dropping is allowed
+    // if ($(event.target).hasClass("leads-status")) {
+    //     event.preventDefault();
+    //     event.dataTransfer.dropEffect = "all";
+    // }
+    // //dropping is NOT allowed
+    // else {
+    //     event.dataTransfer.dropEffect = "none";
+    // }
+    event.preventDefault();
 }
 
-function drop(event) {
-    var data = event.dataTransfer.getData("elementId");
-    var companyId = data.split('-').slice(-1)[0];
-    var statusId = event.target.getAttribute("id").split('-').slice(-1)[0];
+function updateCompanyStatus(event) {
+    var element = event.dataTransfer.getData("elementId");
+    var oldStatusId = event.dataTransfer.getData("oldStatusId");
+    var companyId = element.split('-').slice(-1)[0];
+    var statusId = $(event.target).closest('.leads-status').attr('id').split('-').slice(-1)[0];
+    var loadingIcon = $('<div class="overlay"><i class="fa fa-refresh fa-spin"></i></div>');
     $.ajax({
         url: '/company/update/'+companyId+'/status/'+statusId,
         method: 'PUT',
@@ -27,18 +31,59 @@ function drop(event) {
             statusId: statusId,
         },
         beforeSend: function() {
-            $('body').css('cursor', 'wait');
+            $('.box-header-leads').append(loadingIcon);
         },
         success: function(response) {
             event.preventDefault();
-            $('#'+data).find('.label').html('0 second ago');
-            event.target.appendChild(document.getElementById(data));
+            //move element, increase new count, decrease oldcount, reset time diff label
+            var targetCountLabel = $(event.target).find('.status-label .label');
+            var oldCountLabel = $('#'+oldStatusId).find('.status-label .label');
+            targetCountLabel.html(parseInt(targetCountLabel.html()) + 1);
+            oldCountLabel.html(parseInt(oldCountLabel.html()) - 1);
+            $('#'+element).find('.label').html('0 second ago');
+            $(event.target).closest('.leads-status').append($('#'+element));
         },
         error: function(response, status, error) {
             console.log(error);
         },
         complete: function() {
-            $('body').css('cursor', 'default');
+            $('.box-header-leads').find('.overlay').remove();
+        }
+    });
+}
+
+function updateMembershipStatus(event) {
+    var element = event.dataTransfer.getData("elementId");
+    var oldStatusId = event.dataTransfer.getData("oldStatusId");
+    var companyId = element.split('-').slice(-1)[0];
+    var statusId = event.target.getAttribute("id").split('-').slice(-1)[0];
+    var loadingIcon = $('<div class="overlay"><i class="fa fa-refresh fa-spin"></i></div>');
+    $.ajax({
+        url: '/company/update/'+companyId+'/status/'+statusId,
+        method: 'PUT',
+        dataType:"json",
+        data: {
+            companyId: companyId,
+            statusId: statusId,
+        },
+        beforeSend: function() {
+            $('.box-header-leads').append(loadingIcon);
+        },
+        success: function(response) {
+            event.preventDefault();
+            //move element, increase new count, decrease oldcount, reset time diff label
+            var targetCountLabel = $(event.target).find('.status-label .label');
+            var oldCountLabel = $('#'+oldStatusId).find('.status-label .label');
+            targetCountLabel.html(parseInt(targetCountLabel.html()) + 1);
+            oldCountLabel.html(parseInt(oldCountLabel.html()) - 1);
+            $('#'+element).find('.label').html('0 second ago');
+            event.target.appendChild(document.getElementById(element));
+        },
+        error: function(response, status, error) {
+            console.log(error);
+        },
+        complete: function() {
+            $('.box-header-leads').find('.overlay').remove();
         }
     });
 }
