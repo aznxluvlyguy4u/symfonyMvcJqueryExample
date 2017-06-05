58c8a0675ecde69e7ddb1c86007564f27cfed4ab
Yubin Chen <yubinchen18@gmail.com>
2017-06-05 09:50:44 +0200

    Checkin IvoryCKEditorBundle (symfony wysiwyg editor)

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            new Ivory\CKEditorBundle\IvoryCKEditorBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
