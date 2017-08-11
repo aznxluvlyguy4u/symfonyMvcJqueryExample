28f90be88f4bbaac83009345231d88fe8b8d8974
Johnnie Ho <johnnieho89@gmail.com>
2017-08-11 12:29:59 +0200

    Improve the confirmation dialog
    - Use enums
    - Keep everything consolidated, message string

--- web/js/confirmations/confirm.js
+++ web/js/confirmations/confirm.js
@@ -1,3 +1,12 @@
+/**
+ * This can also be used for other confirmations such as click events, form submissions etc :)
+ *
+ */
+
+var ConfirmEnums = {
+    CLEAR_COMMENTS: 'Do you really want to clear the comments history?'
+};
+
 function confirmationDialog(event, element, message)
 {
     var confirmation = confirm(message);
